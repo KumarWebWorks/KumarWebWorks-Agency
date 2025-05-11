cf164aae4c57914adb43e5b9911cbba92fccf7ce
Kumar Web Works <kumarwebworks@gmail.com>
2025-05-11 10:29:57 +0530

    CEO complete

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,20 +1,46 @@
-import RootLayout from "@/components/RootLayout";
+// import RootLayout from "@/components/RootLayout";
+// import "./globals.css";
+
+// export const metadata = {
+//   title: {
+//     template: "Kumar Web Works",
+//     default: "Kumar Web Works",
+//   },
+// };
+
+// export default function Layout({ children }) {
+//   return (
+//     <html
+//       lang="en"
+//       className="h-full bg-neutral-950 text-base antialiased text-neutral-100"
+//     >
+//       <body className="flex min-h-full flex-col">
+//         <RootLayout>{children}</RootLayout>
+//       </body>
+//     </html>
+//   );
+// }
+// src/app/layout.jsx
+
 import "./globals.css";
+import RootLayout from "@/components/RootLayout";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: {
-    template: "Kumar Web Works",
+    template: "%s | Kumar Web Works",
     default: "Kumar Web Works",
   },
+  description:
+    "Kumar Web Works offers top-notch website and app development using React, Next.js, and Java Spring Boot.",
 };
 
-export default function Layout({ children }) {
+export default function Root({ children }) {
   return (
-    <html
-      lang="en"
-      className="h-full bg-neutral-950 text-base antialiased text-neutral-100"
-    >
-      <body className="flex min-h-full flex-col">
+    <html lang="en" className="h-full bg-neutral-950 text-base antialiased text-neutral-100">
+      <body className={`${inter.className} flex min-h-full flex-col`}>
         <RootLayout>{children}</RootLayout>
       </body>
     </html>
