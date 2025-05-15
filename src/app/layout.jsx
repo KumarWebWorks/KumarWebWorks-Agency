1eb121ada67ea5a44473009549f2990f652fce54
Kumar Web Works <kumarwebworks@gmail.com>
2025-05-15 08:55:45 +0530

    contact form rupees icon updated

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import RootLayout from "@/components/RootLayout";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
