3ff0f39bed320ff289e28c9dfbdddc654cc3d254
Kumar Web Works <kumarwebworks@gmail.com>
2025-05-17 13:32:05 +0530

    blog card is created

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -12,12 +12,12 @@ export default function BlogCard() {
   return (
    <div className="w-full max-w-sm mx-auto rounded-lg shadow-lg bg-white dark:bg-gray-800 ">
         <Card
-      className="max-w-sm bg-neutral-950 text-base antialiased pt-5 pl-5 pr-5 "
+      className="max-w-sm bg-neutral-950 text-base antialiased pt-5 pl-5 pr-5 pb-2"
       renderImage={() => <Image width={500} height={500} src="/hero.jpg" alt="image 1" />}
     >
    
-<div className="">
-  <div className="  rounded-lg shadow-lg">
+<div className=" rounded-lg pb-3">
+  
     <h5 className="  xl:text-2xl  md:text-lg  sm:text-base font-bold tracking-tight  text-slate-100">
         Noteworthy technology acquisitions 2021
       </h5>
@@ -25,7 +25,7 @@ export default function BlogCard() {
         Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
       </p>
       
-  </div>
+  
 </div>
  
  
