53b385658278b7591a65115585654d0d8882662f
Kumar Web Works <kumarwebworks@gmail.com>
2025-05-18 12:53:01 +0530

    ui

--- src/components/GlowingEffectDemoSecond.jsx
+++ src/components/GlowingEffectDemoSecond.jsx
@@ -12,7 +12,7 @@ export function GlowingEffectDemoSecond() {
         icon={<Code className="h-4 w-4 text-black dark:text-neutral-400" />}
          title = "Frontend"
       description = "Articles on React, Vue, and UI design."
-         imageSrc="/laptop.jpg"
+         imageSrc="/frontend-main.jpg"
            imageAlt="Example description"
             />
       <GridItem
@@ -20,7 +20,7 @@ export function GlowingEffectDemoSecond() {
         icon={<Server className="h-4 w-4 text-black dark:text-neutral-400" />}
         title= "Backend"
       description ="Node.js, Spring Boot, Databases."
-         imageSrc="/laptop.jpg"
+         imageSrc="/backend-main.jpg"
            imageAlt="Example description"
         />
       <GridItem
@@ -28,12 +28,12 @@ export function GlowingEffectDemoSecond() {
         icon={<Settings className="h-4 w-4 text-black dark:text-neutral-400" />}
          title = "DevOps"
       description = "Docker, CI/CD, and deployments."
-         imageSrc="/laptop.jpg"
+         imageSrc="/devops-main.jpg"
            imageAlt="Example description"
         />
       <GridItem
         area="md:[grid-area:2/7/3/13] xl:[grid-area:1/8/2/13]"
-         imageSrc="/laptop.jpg"
+         imageSrc="uiux-main.jpg"
            imageAlt="Example description"
         icon={<PenTool className="h-4 w-4 text-black dark:text-neutral-400" />}
          title= "UI/UX"
@@ -43,7 +43,7 @@ export function GlowingEffectDemoSecond() {
         area="md:[grid-area:3/1/4/13] xl:[grid-area:2/8/3/13]"
         icon={<TerminalSquare className="h-4 w-4 text-black dark:text-neutral-400" />}
         title="The best AI code editor ever"
-         imageSrc="/laptop.jpg"
+         imageSrc="/codeeditor-main.jpg"
            imageAlt="Example description"
         description="Experience real-time code suggestions, bug fixes, and smart refactoring—powered by cutting-edge AI. This is the future of development." />
     </ul>
@@ -73,7 +73,7 @@ const GridItem = ({
         <div
           className="relative flex h-full flex-col justify-between gap-6 overflow-hidden rounded-xl p-6 md:p-6 dark:shadow-[0px_0px_27px_0px_#2D2D2D]"
           style={{
-            backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${imageSrc})`,
+            backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.1), rgba(0,0,0,1)), url(${imageSrc})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
@@ -86,7 +86,7 @@ const GridItem = ({
               <h3 className="-tracking-4 pt-0.5 font-sans text-xl/[1.375rem] font-semibold text-balance text-white md:text-2xl/[1.875rem]">
                 {title}
               </h3>
-              <h2 className="font-sans text-sm/[1.125rem] text-neutral-300 md:text-base/[1.375rem] [&_b]:md:font-semibold [&_strong]:md:font-semibold">
+              <h2 className="font-sans text-sm/[1.125rem] text-neutral-200 md:text-base/[1.375rem] [&_b]:md:font-semibold [&_strong]:md:font-semibold">
                 {description}
               </h2>
             </div>
