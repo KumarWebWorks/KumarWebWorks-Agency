aff53c632289aec00dac126b071d53d07422858d
Kumar Web Works <kumarwebworks@gmail.com>
2025-05-22 12:49:40 +0530

    super base congif

--- src/lib/getAllBlogs.js
+++ src/lib/getAllBlogs.js
@@ -6,9 +6,9 @@ import { createClient } from "@supabase/supabase-js";
 // NEXT_PUBLIC_SUPABASE_ANON_KEY = 
 
 
-const supabaseUrl = "https://mpubpekirhwbtmukbxfc.supabase.co";
+const supabaseUrl =process.env.NEXT_PUBLIC_SUPABASE_url;
 
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wdWJwZWtpcmh3YnRtdWtieGZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4MDQyMDcsImV4cCI6MjA2MzM4MDIwN30.HYGZfePbjODoc2aelKtSI1WbnYQiUDxPLQvT2Vv00pM";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 if (!supabaseUrl || !supabaseAnonKey) { 
   throw new Error("Supabase URL or Anon Key is not defined");
 }
