d787bc865b667f7f8658c6f2b1fb159f6eb265c7
Kumar Web Works <kumarwebworks@gmail.com>
2025-05-22 10:02:14 +0530

    super base congif

--- src/lib/getAllBlogs.js
+++ src/lib/getAllBlogs.js
@@ -2,15 +2,17 @@
 
 
 import { createClient } from "@supabase/supabase-js";
+// NEXT_PUBLIC_SUPABASE_url = https://mpubpekirhwbtmukbxfc.supabase.co
+// NEXT_PUBLIC_SUPABASE_ANON_KEY = 
 
 
-// const supabaseUrl = "https://mpubpekirhwbtmukbxfc.supabase.co";
+const supabaseUrl = "https://mpubpekirhwbtmukbxfc.supabase.co";
 
-// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-// if (!supabaseUrl || !supabaseAnonKey) { 
-//   throw new Error("Supabase URL or Anon Key is not defined");
-// }
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_url, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wdWJwZWtpcmh3YnRtdWtieGZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4MDQyMDcsImV4cCI6MjA2MzM4MDIwN30.HYGZfePbjODoc2aelKtSI1WbnYQiUDxPLQvT2Vv00pM";
+if (!supabaseUrl || !supabaseAnonKey) { 
+  throw new Error("Supabase URL or Anon Key is not defined");
+}
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 
 
