6ac29f471ee711ddae6f544aec99fac769bd89cd
Kumar Web Works <kumarwebworks@gmail.com>
2025-05-23 21:03:50 +0530

    blog pages updated

--- src/lib/superbaseConfig.js
+++ src/lib/superbaseConfig.js
@@ -3,14 +3,14 @@ import { createClient } from "@supabase/supabase-js";
 // NEXT_PUBLIC_SUPABASE_ANON_KEY = 
 
 
-// const supabaseUrl =process.env.SUPABASE_url;
-const supabaseUrl ="https://mpubpekirhwbtmukbxfc.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wdWJwZWtpcmh3YnRtdWtieGZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4MDQyMDcsImV4cCI6MjA2MzM4MDIwN30.HYGZfePbjODoc2aelKtSI1WbnYQiUDxPLQvT2Vv00pM";
+ //const supabaseUrl =process.env.SUPABASE_url;
 
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 
-//  const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+  // const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
 
 if (!supabaseUrl || !supabaseAnonKey) { 
