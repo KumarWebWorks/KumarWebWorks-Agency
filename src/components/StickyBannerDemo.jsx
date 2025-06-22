723246b16edc625d7e5196144d42ddc8888b6099
KumarWebWorks <kumarwebworks@gmail.com>
2025-06-22 17:17:52 +0000

    small change on banner

--- src/components/StickyBannerDemo.jsx
+++ src/components/StickyBannerDemo.jsx
@@ -4,18 +4,18 @@ import Link from "next/link";
 
 export function StickyBannerDemo() {
   return (
+    <Link href="/anouncement">
     <div className="relative flex  w-full flex-col overflow-y-auto">
       <StickyBanner className="bg-transparent">
         <p className=" mx-0 max-w-[90%] text-white drop-shadow-md">
           Get a Stunning One-Page Website — Absolutely FREE! &nbsp;&nbsp;&nbsp;
           
-          <Link href="/anouncement" className="transition duration-200 hover:underline">
-          Read announcement
-          </Link>
+          
         </p>
       </StickyBanner>
       
     </div>
+    </Link>
   );
 }
 
